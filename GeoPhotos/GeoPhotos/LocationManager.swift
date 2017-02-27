bcdf8d9b463f80d75efcebf3e772b6e6d76ac926
Nehal Kanetkar <nkanetkar@pelmorex.com>
2017-02-27 12:18:14 -0500

    Add in files for storyboards, location services and view controllers

---
+++ GeoPhotos/GeoPhotos/LocationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocationManager.swift
+//  GeoPhotos
+//
+//  Created by Nehal Kanetkar on 2017-02-27.
+//  Copyright © 2017 GotIt. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager {
+    
+}
